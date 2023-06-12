1658e6bf1bb625c544668f7ee79fb9568f5a64ca
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-06-12 18:47:01 +0100

    Update file 1-element_at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return 0
+        return None
     elif (len(my_list) - 1) < idx:
-        return 0
+        return None
     else:
         return my_list[idx]
