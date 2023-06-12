627ba2600a298feb0e1fd59795e316afac1cc9f1
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-06-12 19:26:33 +0100

    Update the code of File 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     elif (len(my_list) - 1) < idx:
-        return None
+        return my_list
     else:
         new_list = my_list.copy()
         new_list[idx] = element
