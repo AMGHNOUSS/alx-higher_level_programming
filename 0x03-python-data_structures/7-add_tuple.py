ef6ca78956490681e2dda4d9d776978836bee051
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-06-12 23:22:45 +0100

    Update code of File 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,22 +2,22 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     len_a = len(tuple_a)
     len_b = len(tuple_b)
-    if len_a == 0: 
+    if len_a == 0:
         a0 = 0
         a1 = 0
+    elif len_a == 1:
+        a0 = tuple_a[0]
+        a1 = 0
+    else:
+        a0 = tuple_a[0]
+        a1 = tuple_a[1]
     if len_b == 0:
         b0 = 0
         b1 = 0
-    if len_a == 1:
-        a0 = tuple_a[0]
-        a1 = 0
-    if len_b == 1:
+    elif len_b == 1:
         b0 = tuple_b[0]
         b1 = 0
-    if len_a == 2:
-        a0 = tuple_a[0]
-        a1 = tuple_a[1]
-    if len_b == 2:
+    else len_b == 2:
         b0 = tuple_b[0]
         b1 = tuple_b[1]
     new_tuple = (a0 + b0, a1 + b1)
