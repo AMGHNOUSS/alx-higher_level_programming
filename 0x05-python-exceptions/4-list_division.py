ae53196a3f4c2e5adb146ae599d8bb1edd6ad1d9
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-06-26 13:51:12 +0100

    Update the code of File 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except (ValueError, TypeError):
             print("wrong type")
             res = 0
-        except:
+        except ZeroDivisionError:
             print("division by 0")
             res = 0
         finally:
