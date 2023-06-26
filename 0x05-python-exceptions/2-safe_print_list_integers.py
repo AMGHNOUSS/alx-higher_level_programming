7d2b9118afa4114316f513349777febe02c04304
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-06-26 12:40:10 +0100

    Update the code of File 2-safe_print_list-integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            cmp += 1
         except (IndexError, ValueError, TypeError):
             pass
-    print("")
+        else:
+            cmp += 1
+    print()
     return cmp
