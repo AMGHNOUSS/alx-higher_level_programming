b4478f611451ae50135e01ae7ebd49308f98711b
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-06-26 12:24:23 +0100

    Update the code of File 1-safe_print_list.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        n = int(value)
-        print("{:d}".format(n))
+        print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
         return False
