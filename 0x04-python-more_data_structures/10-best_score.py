c919f5a40f9133314b3410885a5a1ed7ed03a3d9
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-23 21:23:07 +0100

    Update The code of File 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    list_keys = list(a_dictionary.keys())
-    big = a_dictionary[list_keys[0]]
-    for i in list_keys:
-        if big < a_dictionary[i]:
-            big = a_dictionary[i]
-            big_key = i
-    return big_key
+    return max(a_dictionary, key=a_dictionary.get)
