2377e0f0f5eae972bc09cf043d9a5177fb4c3472
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-25 16:07:17 +0100

    Create File 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -34,5 +34,5 @@ class Square():
         else:
             for i in range(0, self.__size):
                 for j in range(0, self.__size):
-                    print("#",end="")
+                    print("#", end="")
                 print()
