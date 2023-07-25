60f657440afd05df6d736569eb24d2bcf9ae826b
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-25 16:45:40 +0100

    Creater File 6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Class Square with Attribute size"""
+
+
+class Square():
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @size.setter
+    def size(self, value):
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(value)
+
+    @position.setter
+    def position(self, value):
+        if (type(value) is not tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (len(value) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif ((type(value[1]) is not int) or (type(value[0]) is not int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif ((value[0] < 0) or (value[1] < 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = tuple(value)
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(0, self.__size):
+                for k in range(0, self.__position[0]):
+                    print(" ", end="")
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
