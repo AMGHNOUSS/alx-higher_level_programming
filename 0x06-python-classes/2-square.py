ec1e2efd06b87b1263d8094689881d1abe79fb37
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-25 15:09:39 +0100

    Create File 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Class Square with Attribute size"""
+
+
+class Square():
+    """Attribute size in Class Square"""
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be >= 0")
+        elif (size < 0):
+            raise ValueError("size must be an integer")
+        self.__size = size
