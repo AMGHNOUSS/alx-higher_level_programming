46fba6215573037b7ab5fd79acd1ee52f877791d
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-25 15:14:16 +0100

    Update code of File 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,4 +9,5 @@ class Square():
             raise TypeError("size must be >= 0")
         elif (size < 0):
             raise ValueError("size must be an integer")
-        self.__size = size
+        else:
+            self.__size = size
