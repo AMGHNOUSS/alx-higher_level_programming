dffff76cf5abce375f9deafd57d78142261001d8
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-12 14:43:27 +0100

    Fix error

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-function add(a, b) { return(a + b); }
+function add (a, b) { return (a + b); }
 const { argv } = require('process');
 const a = parseInt(argv[2]);
 const b = parseInt(argv[3]);
