1412eaa5481256c55fde7ce240d470a5146dee80
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-13 00:50:10 +0100

    Create File 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Square;
