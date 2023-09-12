acb3b560288925c2dff655400901b9a63fc36a6f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-12 02:34:14 +0100

    Create fix error

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const number = parseInt(argv[2]);
+if (number) { for (let i = 0; i < number; i++) { for (let j = 0; j < number; j++) { process.stdout.write('X'); } console.log() } } else { console.log('Missing size'); }
