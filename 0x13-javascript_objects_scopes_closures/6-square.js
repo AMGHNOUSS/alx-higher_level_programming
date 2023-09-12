c62b4e4154dbc32701f49af4f7333e75643055e5
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-13 00:54:14 +0100

    Update File 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const SquareOld = require('./5-square.js');
+class Square extends SquareOld {
   charPrint(c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
+      for (let j = 0; j < this.height; j++) {
         process.stdout.write(c);
       }
       console.log();
