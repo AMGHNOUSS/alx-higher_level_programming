734bf048b3ed3c316985f12982214017c584f132
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-12 14:23:56 +0100

    Create File 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial(n) {
+	if (n === NaN)
+		return (1);
+	else if (n < 2)
+		return (1);
+	else
+		return(n * factorial(n - 1));
+}
+const { argv } = require('process');
+const a = parseInt(argv[2]);
+if (a) { console.log(factorial(a)); } else { console.log('1'); }
