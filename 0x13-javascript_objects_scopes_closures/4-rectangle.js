568598775e4916310868b547a76c12dfdcee3c81
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-13 00:34:26 +0100

    Create File 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print (w, h) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  rotate () {
+    const a = this.width;
+    this.width = this.height;
+    this.height = a;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
