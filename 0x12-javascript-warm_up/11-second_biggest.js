a4f81de950627b33805aef49e764969e39124142
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-12 15:38:19 +0100

    Fix error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (argv.length === 2 || argv.length === 3) { console.log(0); }
-const array = [];
-for (let i = 2; i < argv.length; i++) {
-  array[i - 2] = parseInt(argv[i]);
+if (argv.length === 2 || argv.length === 3) {
+  console.log(0);
 }
-const max = Math.max(...array);
-let second;
-second = array[0];
-for (let i = 0; i < array.length; i++) {
-  if (array[i] > second && max > array[i]) { second = array[i]; }
+if (argv.length > 3) {
+  const array = [];
+  for (let i = 2; i < argv.length; i++) { array[i - 2] = parseInt(argv[i]); }
+  const max = Math.max(...array);
+  let second;
+  second = array[0];
+  for (let i = 0; i < array.length; i++) { if (array[i] > second && max > array[i]) { second = array[i]; } }
+  console.log(second);
 }
-console.log(second);
