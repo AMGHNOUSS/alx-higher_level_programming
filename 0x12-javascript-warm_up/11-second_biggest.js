92d731aefbf9a2f40605f4d3dbccc9a2677efebe
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-12 16:49:05 +0100

    Fix error 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
+function second_biggest() {
 const { argv } = require('process');
 if (argv.length === 2 || argv.length === 3) {
-  console.log(0);
+  return(0);
+  }
+  if (argv.length > 3) {
+    const array = [];
+    for (let i = 2; i < argv.length; i++) { array[i - 2] = parseInt(argv[i]); }
+    const max = Math.max(...array);
+    let second;
+    second = array[0];
+    for (let i = 0; i < array.length; i++) { if (array[i] > second && max > array[i]) { second = array[i]; } }
+    return (second);
+  }
 }
-if (argv.length > 3) {
-  const array = [];
-  for (let i = 2; i < argv.length; i++) { array[i - 2] = parseInt(argv[i]); }
-  const max = Math.max(...array);
-  let second;
-  second = array[0];
-  for (let i = 0; i < array.length; i++) { if (array[i] > second && max > array[i]) { second = array[i]; } }
-  console.log(second);
-}
+if (second_biggest()) { console.log(second_biggest()); } else { console.log(0) }
