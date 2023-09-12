387cb22f36da064a1cf1d89a2eeefd1335b5ca5f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-12 15:29:51 +0100

    Create File 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (argv.length === 2 || argv.length === 3)
-	console.log(0)
+if (argv.length === 2 || argv.length === 3) { console.log(0); }
+const array = [];
+for (let i = 2; i < argv.length; i++) {
+  array[i - 2] = parseInt(argv[i]);
+}
+const max = Math.max(...array);
+let second;
+second = array[0];
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > second && max > array[i]) { second = array[i]; }
+}
+console.log(second);
