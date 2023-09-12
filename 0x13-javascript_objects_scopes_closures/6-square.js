e215d088257e504620fc9469661e053ed6743387
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-13 00:56:39 +0100

    Update File 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const SquareOld = require('./5-square.js');
 class Square extends SquareOld {
-  charPrint(c) {
-    if (c === undefined) {
-      c = 'X';
-    }
+  charPrint (c) {
+    if(c === undefined) { c = 'X'; }
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.height; j++) {
         process.stdout.write(c);
