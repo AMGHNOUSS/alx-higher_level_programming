154799984280bb7a77e49eaf00a7d186c04946ca
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-14 01:05:18 +0100

    Create File 101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+let dictN = {}
+for (const key in dict) {
+  if (dictN[dict[key]] === undefined) {
+    dictN[dict[key]] = [key];
+  }
+  else {
+    dictN[dict[key]].push(key);
+  }
+}
+console.log(dictN);
