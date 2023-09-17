55a1f69285c6ec8b5331aeca2be13ef0d318bcef
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-17 22:45:45 +0100

    Create File 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
         port=3306
     )
     c = db.cursor()
-    c.execute("SELECT cities.id, cities.name, states.name FROM cities, \
-        states WHERE states.id = cities.state_id AND states.name = \
-        '{}';".format(sys.argv[4]))
+    c.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN \
+        states ON states.id = cities.state_id WHERE \
+        states.name = '{}';".format(sys.argv[4]))
     for state in c.fetchall():
         print(state)
