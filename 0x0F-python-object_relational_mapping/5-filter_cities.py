b0240a622709abc9b559dbccd901ed1eef8ab36d
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-17 22:41:03 +0100

    Create File 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Display all cities from the database hbtn_0e_4_usa."""
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    c = db.cursor()
+    c.execute("SELECT cities.id, cities.name, states.name FROM cities, \
+        states WHERE states.id = cities.state_id and states.name = \
+        '{}';".format(sys.argv[4]))
+    for state in c.fetchall():
+        print(state)
