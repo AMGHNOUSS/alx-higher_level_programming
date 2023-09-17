d0ae08b8e51f3635b4ff3d0212e9269c7daaf08a
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-17 21:27:03 +0100

    Update File 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
         port=3306
     )
     c = db.cursor()
-    c.execute("SELECT * FROM `states` WHERE name = `{}` ORDER BY states.id ASC".format(sys.argv[4]).strip("'"))
+    c.execute("""SELECT * FROM states
+                WHERE name LIKE BINARY '{}'
+                ORDER BY states.id ASC""".format(sys.argv[4]).strip("'"))
     for state in c.fetchall():
         print(state)
