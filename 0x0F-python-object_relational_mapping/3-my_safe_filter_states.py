61c28aca935b430fae55ac6be1c384bd6e7292ef
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-17 21:50:18 +0100

    Create File 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Display all values in the states table where name matches the argument."""
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    for state in c.fetchall():
+        print(state)
