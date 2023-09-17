accb4565f534ffd387d702f2878753602535dca7
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-17 14:11:04 +0100

    Fix coding style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,9 +4,9 @@ import sys
 import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
+        user = sys.argv[1],
+        passwd = sys.argv[2],
+        db = sys.argv[3],
         port = 3306
     )
     c = db.cursor()
