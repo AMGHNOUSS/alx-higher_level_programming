f6e94cb398ee2b2ad95b26d805f4bd0c0292c8c9
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-17 14:20:36 +0100

    Update code 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,9 +10,10 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        port = 3306
+        port=3306
     )
     c = db.cursor()
-    c.execute("SELECT * FROM `states` where name = ^N")
+    c.execute("SELECT * FROM `states`")
     for state in c.fetchall():
-        print(state)
+        if state[1][0] == 'N':
+            print(state)
