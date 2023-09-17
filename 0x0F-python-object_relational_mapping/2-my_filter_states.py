b1c9245969db979caae8b0154a2c5cbac640f2a1
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-17 21:22:28 +0100

    Update File 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         port=3306
     )
     c = db.cursor()
-    c.execute("SELECT * FROM `states` where name = `{}`".format(sys.argv[3]))
+    c.execute("SELECT * FROM `states` WHERE name = `{}`".format(sys.argv[4]))
     for state in c.fetchall():
         print(state)
