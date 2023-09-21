17bd793d1aa615fb93f4a0f6b403c854c64adfaf
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-22 00:25:55 +0100

    Update File 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name == sys.argv[4]).first()
-    if states:
-        print("{}".format(state.id))
+    state = session.query(State).filter(State.name == sys.argv[4]).first()i
+    if state:
+        print(state.id)
     else:
         print("Not found")
