fd00d31f899a6b6b90319e74bc02b123201a4b09
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-21 23:44:55 +0100

    Create File 9-model_state_filter_a.py

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa."""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(name == "%a%").all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
