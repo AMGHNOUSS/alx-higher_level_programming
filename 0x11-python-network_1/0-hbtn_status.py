95d0e08e0a24be4799a9b6c3758f3bed4399a3e0
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-30 01:07:40 +0100

    Update File 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
+from urllib import request
 
-
-import urllib.request
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
-        print('    - type: {}'.format(type(html)))
-        print('    - content: {}'.format(html))
-        print('    - utf8 content: {}'.format(html.decode("utf-8")))
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
