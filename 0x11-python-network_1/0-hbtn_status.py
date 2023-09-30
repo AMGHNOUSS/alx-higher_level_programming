72acf55d94d14c9683fe5f4b4bc1c4aa68b43f48
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-30 01:02:06 +0100

    Update File 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
+"""Python script that fetches https://intranet.hbtn.io/status"""
+
+
 import urllib.request
-
-
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
