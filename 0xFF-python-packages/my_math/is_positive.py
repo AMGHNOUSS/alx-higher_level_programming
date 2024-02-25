6876ef76750973a562006521b33aac85c1dcc200
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-02-25 23:33:42 +0100

    is positive

---
+++ 0xFF-python-packages/my_math/is_positive.py
@@ -0,0 +1,7 @@
+
+
+def my_is_positive(a):
+    if (a >= 0):
+        return True
+    else:
+        return False
--- 0xFF-python-packages/my_script.py
+++ 0xFF-python-packages/my_script.py
@@ -1,5 +1,5 @@
 
-from my_math import abs
+from my_math import abs, is_positive
 from my_math.functions import add, mul, div, sub
 
 a = 10
@@ -12,3 +12,5 @@ print(f"{a} - {b} = {sub.my_sub(a, b)}")
 print(f"{a} * {b} = {mul.my_mul(a, b)}")
 print(f"{a} / {b} = {div.my_div(a, b)}")
 
+print(f"{c} is positive : {is_positive.my_is_positive(c)}")
+print(f"{a} = {is_positive.my_is_positive(a)}")
