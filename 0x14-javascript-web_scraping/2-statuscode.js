06927cbfdcbc23baf4d7958a7bb942c930503cd0
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-03-27 01:46:05 +0000

    Create File 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Display the status code of a `GET` request.
+const request = require('request');
+request.get(argv[2]).on('response', function(response) {
+  console.log("code : " + response.statusCode);
+});
