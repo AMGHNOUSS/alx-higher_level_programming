3a53b49495ce72738fd530841129466c0872c909
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-03-27 14:03:34 +0000

    Update File 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Print The Number of a Star Wars movie.
 const request = require('request');
-request(process.argv[2], function(error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1 
+        ? count + 1
         : count;
     }, 0));
   }
